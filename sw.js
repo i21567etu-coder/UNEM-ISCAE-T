ac2ea4d4d7f717b7c9998878a9a388db8328fc3a
i21567etu-coder <i21567.etu@iscae.mr>
2026-04-02 20:18:58 +0000

    Change CACHE_NAME to 'unem-iscae-v15'
    Updated cache name to trigger new cache for users.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // قم بتغيير هذا الرقم في كل مرة تقوم بتحديث الموقع ليظهر الإشعار للمستخدمين
-const CACHE_NAME = 'unem-iscae-v14';
+const CACHE_NAME = 'unem-iscae-v15';
 
 const urlsToCache =[
   './',
