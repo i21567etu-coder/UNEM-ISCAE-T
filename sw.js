363d0148022328131e15ae395606579b0dea7c5a
i21567etu-coder <i21567.etu@iscae.mr>
2026-04-05 16:08:36 +0000

    Change cache name to unem-iscae-V25
    Updated cache name for service worker to V25.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // قم بتغيير هذا الرقم في كل مرة تقوم بتحديث الموقع ليظهر الإشعار للمستخدمين
-const CACHE_NAME = 'unem-iscae-V24';
+const CACHE_NAME = 'unem-iscae-V25';
 
 const urlsToCache =[
   './',
